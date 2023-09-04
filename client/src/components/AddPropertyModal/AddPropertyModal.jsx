dab12fa8f4956fd8b94229f5c6e4c308241fcec1
adeleyepaul <adeleyepaul2@gmail.com>
2023-09-04 20:28:37 +0100

    auth fixed

--- client/src/components/AddPropertyModal/AddPropertyModal.jsx
+++ client/src/components/AddPropertyModal/AddPropertyModal.jsx
@@ -1,14 +1,14 @@
 import { Container, Modal, Stepper } from "@mantine/core";
 import React, { useState } from "react";
 import AddLocation from "../AddLocation/AddLocation";
-// import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 import UploadImage from "../UploadImage/UploadImage";
 import BasicDetails from "../BasicDetails/BasicDetails";
 import Facilities from "../Facilities/Facilities";
 
 const AddPropertyModal = ({ opened, setOpened }) => {
   const [active, setActive] = useState(0);
-  // const { user } = useAuth0();
+  const { user } = useAuth0();
 
   const [propertyDetails, setPropertyDetails] = useState({
     title: "",
